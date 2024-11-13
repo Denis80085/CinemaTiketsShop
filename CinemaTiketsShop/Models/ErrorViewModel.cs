23f879bbcdba9865c2de849d8f0af0b45143ecf4
denis <denisermurachi06@gmail.com>
2024-11-13 20:56:13 +0100

    Add project files.

---
+++ CinemaTiketsShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaTiketsShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
